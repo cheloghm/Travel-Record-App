ee4c4686530879d0bfb023dd90175da0fb993948
graham chukwumaobi <cheloghm@gmail.com>
2021-03-04 14:16:04 +0000

    Create URL Rest request

---
+++ TravelRecordApp/TravelRecordApp/Helpers/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TravelRecordApp.Helpers
+{
+    public class Constants
+    {
+        public const string VENUE_SEARCH = "https://api.foursquare.com/v2/venues/search?ll={0},{1}&client_id={2}&client_secret={3}&v={4}";
+        public const string CLIENT_ID = "ENSF2TFUVRACSF0NWXRLVSUCTEIZFVI14JRTSB515ZPMILTT";
+        public const string CLIENT_SECRET = "VEX5MULZTY550IXSHGZVKUD1OGV4IUTZRJQCVDA3OZMNGXCE";
+    }
+}
