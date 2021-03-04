9457a8bb52f5f91fe0eedc9462141e9c1ca83897
graham chukwumaobi <cheloghm@gmail.com>
2021-03-04 15:19:18 +0000

    Evaluating the Json

--- TravelRecordApp/TravelRecordApp/NewTravelPage.xaml.cs
+++ TravelRecordApp/TravelRecordApp/NewTravelPage.xaml.cs
@@ -3,9 +3,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using TravelRecordApp.Logic;
 using TravelRecordApp.Model;
+using Xamarin.Essentials;
 using Xamarin.Forms;
+using Xamarin.Forms.Maps;
 using Xamarin.Forms.Xaml;
 
 namespace TravelRecordApp
@@ -13,9 +17,29 @@ namespace TravelRecordApp
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class NewTravelPage : ContentPage
     {
+        ILocationUpdateService loc;
         public NewTravelPage()
         {
             InitializeComponent();
+        }
+
+        CancellationTokenSource cts;
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            var request = new GeolocationRequest(GeolocationAccuracy.Medium, TimeSpan.FromSeconds(10));
+            cts = new CancellationTokenSource();
+            var location = await Geolocation.GetLocationAsync(request, cts.Token);
+            loc = DependencyService.Get<ILocationUpdateService>();
+            Position p;
+            loc.LocationChanged += (object sender, ILocationEvenArgs args) =>
+            {
+                p = new Position(args.Latitude, args.Longitude);
+                var venues = VenueLogic.GetVenues(p.Latitude, p.Longitude);
+            };
+            //var venues = VenueLogic.GetVenues(p.Latitude, p.Longitude);
         }
 
         private void ToolbarItem_Clicked(object sender, EventArgs e)
